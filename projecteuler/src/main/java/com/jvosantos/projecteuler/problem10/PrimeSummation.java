3ee28e123035ff0ea90c67b7e84245b9abdd7ef6
Vasco Santos <vasco.santos@inw-tech.com>
2015-03-27 18:22:29 +0000

    Added some skeleton problems and descriptions

---
+++ projecteuler/src/main/java/com/jvosantos/projecteuler/problem10/PrimeSummation.java
@@ -0,0 +1,42 @@
+/*
+ * Summation of primes
+ * Problem 10
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * 
+ * Find the sum of all the primes below two million.
+ */
+ 
+
+package com.jvosantos.projecteuler.problem10;
+
+import java.util.Arrays;
+
+public class PrimeSummation {
+    boolean[] primes; 
+
+    public PrimeSummation(long n) {
+        primes = new boolean[n];
+        initPrimeTable();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public boolean isPrime(int n) {
+        return primes[n];
+    }
+    
+    private void initPrimeTable() {
+        Arrays.fill(primes,true);        // assume all numbers are prime.
+        primes[0]=primes[1]=false;       // 0 and 1 are not prime.
+        for (int i=2;i<primes.length;i++) {
+            //if the number is prime, go through all its multiples mark as not prime false.
+            if(primes[i]) {
+                for (int j=2;i*j<primes.length;j++) {
+                    primes[i*j]=false;
+                }
+            }
+        }
+    }
+}
